8d0b41aa0a6895168f92679119b5d8ff06ed9d3e
Gabe Fontanilla <gfontan1@my.hpu.edu>
2026-08-21 11:30:01 -1000

    init commit

---
+++ src/content.config.ts
@@ -0,0 +1,33 @@
+import { defineCollection, reference } from "astro:content"
+import { glob } from "astro/loaders"
+import { z } from "astro/zod"
+
+const albums = defineCollection({
+  loader: glob({
+    base: "./src/content/albums",
+    pattern: "**/*.json",
+  }),
+  schema: z.object({
+    title: z.string(),
+    artist: z.string(),
+    releaseDate: z.coerce.date().optional(),
+    cover: z.string().optional(),
+  }),
+})
+
+const tracks = defineCollection({
+  loader: glob({
+    base: "./src/content/tracks",
+    pattern: "**/*.json",
+  }),
+
+  schema: z.object({
+    title: z.string(),
+    album: reference("albums"),
+    trackNumber: z.number().int().positive(),
+    duration: z.string().optional(),
+    audioUrl: z.string().optional(),
+  }),
+})
+
+export const collections = { albums, tracks }
