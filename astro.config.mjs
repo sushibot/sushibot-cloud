b9b323996fd539e8b3315be64dcac30b3aebd57c
Gabe Fontanilla <gfontan1@my.hpu.edu>
2026-09-08 13:47:46 -1000

    cleanup player bar

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
-  server: { port: 3000 },
+	devToolbar: {
+		enabled: false
+	},
+	server: { port: 3000 },
 })
